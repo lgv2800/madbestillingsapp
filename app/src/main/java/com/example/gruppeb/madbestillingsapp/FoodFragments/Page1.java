8625cfaddad6b1f14c99cf789e9841a8c15c368b
sebdtu <32480015+sebdtu@users.noreply.github.com>
2018-10-29 13:30:13 +0100

    Fragment Layout and initialize buttons for page1

--- app/src/main/java/com/example/gruppeb/madbestillingsapp/FoodFragments/Page1.java
+++ app/src/main/java/com/example/gruppeb/madbestillingsapp/FoodFragments/Page1.java
@@ -5,10 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.gruppeb.madbestillingsapp.R;
 
-public class Page1 extends Fragment {
+public class Page1 extends Fragment implements View.OnClickListener {
 
     public Page1() {
         //empty constructor
@@ -24,8 +25,22 @@ public class Page1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View view = inflater.inflate(R.layout.activity_page1, container, false);
+        Button lystBrød = (Button) view.findViewById(R.id.button2);
+        lystBrød.setOnClickListener(this);
+        Button MørktBrød = (Button) view.findViewById(R.id.button3);
+        MørktBrød.setOnClickListener(this);
 
 
         return view;
+    }
+    @Override
+    public void onClick(final View v){
+        switch (v.getId()){
+            case R.id.button2:
+                Button lystBrød = getView().findViewById(R.id.button2);
+                break;
+            case R.id.button3:
+                break;
+        }
     }
 }
