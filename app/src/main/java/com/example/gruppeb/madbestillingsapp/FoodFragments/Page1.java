102ccad3f379adbf78f37cb5840aee9b8654d911
sebdtu <32480015+sebdtu@users.noreply.github.com>
2018-10-29 17:19:22 +0100

    ButtonPress changes color

--- app/src/main/java/com/example/gruppeb/madbestillingsapp/FoodFragments/Page1.java
+++ app/src/main/java/com/example/gruppeb/madbestillingsapp/FoodFragments/Page1.java
@@ -1,5 +1,6 @@
 package com.example.gruppeb.madbestillingsapp.FoodFragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -27,19 +28,28 @@ public class Page1 extends Fragment implements View.OnClickListener {
         View view = inflater.inflate(R.layout.activity_page1, container, false);
         Button lystBrød = (Button) view.findViewById(R.id.button2);
         lystBrød.setOnClickListener(this);
-        Button MørktBrød = (Button) view.findViewById(R.id.button3);
-        MørktBrød.setOnClickListener(this);
+        Button mørktBrød = (Button) view.findViewById(R.id.button3);
+        mørktBrød.setOnClickListener(this);
 
 
         return view;
     }
     @Override
     public void onClick(final View v){
+        Button lystBrød = getView().findViewById(R.id.button2);
+        Button mørktBrød = getView().findViewById(R.id.button3);
         switch (v.getId()){
             case R.id.button2:
-                Button lystBrød = getView().findViewById(R.id.button2);
+                lystBrød.setTextColor(Color.WHITE);
+                lystBrød.setBackgroundColor(Color.BLACK);
+                mørktBrød.setTextColor(Color.BLACK);
+                mørktBrød.setBackgroundColor(Color.WHITE);
                 break;
             case R.id.button3:
+                mørktBrød.setTextColor(Color.WHITE);
+                mørktBrød.setBackgroundColor(Color.BLACK);
+                lystBrød.setTextColor(Color.BLACK);
+                lystBrød.setBackgroundColor(Color.WHITE);
                 break;
         }
     }
