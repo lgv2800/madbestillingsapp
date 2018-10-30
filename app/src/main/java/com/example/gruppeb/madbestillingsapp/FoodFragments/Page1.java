a54183cda04241e51634139e9cc7994be2218d60
Oliver <32456406+outrageousbog@users.noreply.github.com>
2018-10-30 10:50:32 +0100

    init cardview edit

--- app/src/main/java/com/example/gruppeb/madbestillingsapp/FoodFragments/Page1.java
+++ app/src/main/java/com/example/gruppeb/madbestillingsapp/FoodFragments/Page1.java
@@ -26,31 +26,11 @@ public class Page1 extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View view = inflater.inflate(R.layout.activity_page1, container, false);
-        Button lystBrød = (Button) view.findViewById(R.id.button2);
-        lystBrød.setOnClickListener(this);
-        Button mørktBrød = (Button) view.findViewById(R.id.button3);
-        mørktBrød.setOnClickListener(this);
-
 
         return view;
     }
     @Override
     public void onClick(final View v){
-        Button lystBrød = getView().findViewById(R.id.button2);
-        Button mørktBrød = getView().findViewById(R.id.button3);
-        switch (v.getId()){
-            case R.id.button2:
-                lystBrød.setTextColor(Color.WHITE);
-                lystBrød.setBackgroundColor(Color.BLACK);
-                mørktBrød.setTextColor(Color.BLACK);
-                mørktBrød.setBackgroundColor(Color.WHITE);
-                break;
-            case R.id.button3:
-                mørktBrød.setTextColor(Color.WHITE);
-                mørktBrød.setBackgroundColor(Color.BLACK);
-                lystBrød.setTextColor(Color.BLACK);
-                lystBrød.setBackgroundColor(Color.WHITE);
-                break;
-        }
+
     }
 }
