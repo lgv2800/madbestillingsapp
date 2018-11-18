297b04fa62f59ddb9a293d2ba91675be65ab3f28
Oliver <oliverskoeppen@gmail.com>
2018-11-18 19:25:52 +0100

    Fixed bug in DishFactory
    The bugged  crashed the app - changed mehotd

--- app/src/main/java/com/example/gruppeb/madbestillingsapp/Domain/Dishes/DishFactory.java
+++ app/src/main/java/com/example/gruppeb/madbestillingsapp/Domain/Dishes/DishFactory.java
@@ -26,16 +26,15 @@ private SharedPreferences.Editor orderPref, countEdit;
         orderPref = context.getSharedPreferences(context.getString(R.string.current_order_pref),Context.MODE_PRIVATE).edit();
         Dish a;
         switch(dishNumber){
-            case 0: orderPref.putString(context.getString(R.string.page1_food_title),context.getString(count)).apply(); a = new DishOne(); return a;
-            case 1: orderPref.putString(context.getString(R.string.page2_food_title),context.getString(count)).apply(); a = new DishTwo(); return a;
-            case 2: orderPref.putString(context.getString(R.string.page3_food_title),context.getString(count)).apply(); a = new DishThree(); return a;
-            case 3: orderPref.putString(context.getString(R.string.page4_food_title),context.getString(count)).apply(); a = new DishFour(); return a;
-            case 4: orderPref.putString(context.getString(R.string.page5_food_title),context.getString(count)).apply(); a = new DishFive(); return a;
+            case 0: orderPref.putString(context.getString(R.string.page1_food_title),Integer.toString(count)).apply(); a = new DishOne(); return a;
+            case 1: orderPref.putString(context.getString(R.string.page2_food_title),Integer.toString(count)).apply(); a = new DishTwo(); return a;
+            case 2: orderPref.putString(context.getString(R.string.page3_food_title),Integer.toString(count)).apply(); a = new DishThree(); return a;
+            case 3: orderPref.putString(context.getString(R.string.page4_food_title),Integer.toString(count)).apply(); a = new DishFour(); return a;
+            case 4: orderPref.putString(context.getString(R.string.page5_food_title),Integer.toString(count)).apply(); a = new DishFive(); return a;
         }
 
         countEdit.putInt("count", temp).apply();
 
         return null;
     }
-
 }
